2de18dcea418bf47e92df06978293f8a363c39f1
kittrz <kittrz@kittrz.gay>
2023-06-22 07:49:40 -0500

    compile parts of the game into shared libraries because it seems like it'd be useful later

--- premake5.lua
+++ premake5.lua
@@ -11,30 +11,129 @@ workspace "openGL-test"
 		system "linux"
 	filter{}
 
-project "openGL-test"
-	kind "ConsoleApp"
+--project "openGL-test"
+--	kind "ConsoleApp"
+--	language "C"
+--	targetdir "build/%{cfg.buildcfg}"
+--
+--	files { "src/**.h", "src/**.c" }
+--
+--	-- probably unnecessary to have it specify the architecture but idk maybe I want to make this compile for arm at some point
+--	filter { "platforms:Windows" }
+--		architecture "x86_64"
+--		links { "glew32", "glu32", "opengl32", "glfw3", "portaudio", "m"}
+--	
+--	filter { "platforms:Linux" }
+--		architecture "x86_64"
+--		links { "GLEW", "EGL", "GL", "GLU", "OpenGL", "glfw", "portaudio", "m"}
+--	
+--	filter {}
+--
+--	includedirs { "src/engine/stb_image/", "src/engine/resourceLoaders/", "src/engine/", "src/game/", "src/game/gameStates"}
+--
+--	defines { "EXECUTABLE_NAME=\"openGL-test\"" }
+--
+--	filter "configurations:Debug"
+--		symbols "On"
+--	
+--	filter "configurations:Release"
+--		optimize "Speed"
+
+project "stb_image"
+	kind "SharedLib"
 	language "C"
 	targetdir "build/%{cfg.buildcfg}"
 
-	files { "src/**.h", "src/**.c" }
+	-- moved into a seperate directory so that the engine can just have "src/engine/*.h" instead of manually putting every new file
+	files { "src/engine/stb_image/**.h", "src/engine/stb_image/**.c" }
 
-	-- probably unnecessary to have it specify the architecture but idk maybe I want to make this compile for arm at some point
 	filter { "platforms:Windows" }
 		architecture "x86_64"
-		links { "glew32", "glu32", "opengl32", "glfw3", "portaudio", "m"}
-	
+
 	filter { "platforms:Linux" }
 		architecture "x86_64"
-		links { "GLEW", "EGL", "GL", "GLU", "OpenGL", "glfw", "portaudio", "m"}
 	
 	filter {}
 
-	includedirs { "src/engine/resourceLoaders/", "src/engine/", "src/game/", "src/game/gameStates"}
+	includedirs { "src/engine/stb_image/", "src/engine/" }
 
-	defines { "EXECUTABLE_NAME=\"openGL-test\"" }
+	filter "configurations:Release"
+		optimize "Speed"
+
+project "resourceLoaders"
+	kind "SharedLib"
+	language "C"
+	targetdir "build/%{cfg.buildcfg}"
+
+	files { "src/engine/resourceLoaders/**.h", "src/engine/resourceLoaders/**.c" }
+
+	filter { "platforms:Windows" }
+		architecture "x86_64"
+		links { "glew32", "glu32", "opengl32", "glfw3", "portaudio", "m", "stb_image" }
+
+	filter { "platforms:Linux" }
+		architecture "x86_64"
+		links { "GLEW", "EGL", "GL", "GLU", "OpenGL", "glfw", "portaudio", "m", "stb_image"}
+	
+	filter {}
+
+	includedirs { "src/engine/stb_image", "src/engine/resourceLoaders/", "src/engine/" }
 
 	filter "configurations:Debug"
 		symbols "On"
 	
 	filter "configurations:Release"
 		optimize "Speed"
+
+project "engine"
+	kind "SharedLib"
+	language "C"
+	targetdir "build/%{cfg.buildcfg}"
+
+	files { "src/engine/*.h", "src/engine/*.c" }
+
+	filter { "platforms:Windows" }
+		architecture "x86_64"
+		links { "glew32", "glu32", "opengl32", "glfw3", "portaudio", "m" }
+
+	filter { "platforms:Linux" }
+		architecture "x86_64"
+		links { "GLEW", "EGL", "GL", "GLU", "OpenGL", "glfw", "portaudio", "m"}
+	
+	filter {}
+
+	includedirs { "src/engine/resourceLoaders/", "src/engine/", "src/game/" }
+
+	filter "configurations:Debug"
+		symbols "On"
+	
+	filter "configurations:Release"
+		optimize "Speed"
+
+project "game"
+	kind "WindowedApp"
+	language "C"
+	targetdir "build/%{cfg.buildcfg}"
+
+	targetname ("%{wks.name}")
+
+	files { "src/game/**.h", "src/game/**.c" }
+
+	filter { "platforms:Windows" }
+		architecture "x86_64"
+		links { "glew32", "glu32", "opengl32", "glfw3", "portaudio", "m", "engine", "resourceLoaders" }
+
+	filter { "platforms:Linux" }
+		architecture "x86_64"
+		links { "GLEW", "EGL", "GL", "GLU", "OpenGL", "glfw", "portaudio", "m", "engine", "resourceLoaders" }
+	
+	filter {}
+
+	includedirs { "src/engine/", "src/game/", "src/game/gameStates" }
+
+	filter "configurations:Debug"
+		symbols "On"
+	
+	filter "configurations:Release"
+		optimize "Speed"
+
